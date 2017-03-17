22c69919de38ecf0db335ec7a6a85faccbf80cfd
Guillaume Lafargue <guillaume@newzulu.com>
2017-03-17 17:00:56 +0100

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='objectpath',
+    version="0.1",
+    description="objectpath language processor",
+    author='Guillaume Lafargue',
+    author_email='guillaume@newzulu.com',
+    packages=["objectpath", "objectpath.core", "objectpath.utils"]
+)
